ab6a712a1d49c68712a385618937f41b78d4eb4b
Steven Randolph <srandolph6969@gmail.com>
2024-01-12 20:15:48 +0000

    initial commit

---
+++ gitpush.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd $1
+
+git config --global user.email "srandolph6969@gmail.com"
+git config --global user.name "Steven Randolph"
+ 
+echo init the repository
+git init .
+
+echo add all files from root dir below, with ignore dirs and files in the .gitignore
+git add .
+
+echo 'commit all the files'
+git commit -m "initial commit"
+
+echo 'add a remote pseudo for the openadr repository'
+
+git remote add origin https://complexmathguy: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@github.com/complexmathguy/openadr
+
+echo 'push the commits to the repository master'
+git push origin master
+
+
+echo 'add tag newest'
+git tag newest
+
+echo 'push tag newest'
+git push origin newest
+
